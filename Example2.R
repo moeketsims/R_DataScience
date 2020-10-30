78e024fda72f5bc05d1d3594ec49e8de406c9265
moeketsims <bricoleurmosia@gmail.com>
2020-10-30 14:01:37 +0200

    added select, filter and mutate function on my project

--- Example2.R
+++ Example2.R
@@ -1,2 +1,17 @@
-#import dependencies
+
 library(tidyverse)
+library(readxl)
+
+df <- read_excel("data.xlsx")
+
+df_new <- df %>% select(Year, SPI_Kubuta, SPI_Nhlangano) %>% 
+  filter(Year > 1986, SPI_Kubuta < 0) %>% 
+  mutate(col_no1 = SPI_Kubuta/4) %>% 
+  mutate(col_no2 =  col_no1 * SPI_Nhlangano, .after=SPI_Kubuta)
+
+
+
+
+
+
+
