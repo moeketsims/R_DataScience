c111207b4f37c25131fdf9b0fe00c2136e5eda7e
moeketsims <bricoleurmosia@gmail.com>
2020-11-02 19:00:51 +0200

    Updated code

--- Example2.R
+++ Example2.R
@@ -1,17 +1,47 @@
 
 library(tidyverse)
 library(readxl)
+library(ggplot2)
 
-df <- read_excel("data.xlsx")
+df <- tibble(read.csv("train.csv", sep = ";"))
+df$Survived <- as.factor(df$Survived)
+df$Pclass <- as.factor(df$Pclass)
 
-df_new <- df %>% select(Year, SPI_Kubuta, SPI_Nhlangano) %>% 
-  filter(Year > 1986, SPI_Kubuta < 0) %>% 
-  mutate(col_no1 = SPI_Kubuta/4) %>% 
-  mutate(col_no2 =  col_no1 * SPI_Nhlangano, .after=SPI_Kubuta)
+# df$Age[which(is.na(df$Age))] = 0 
+# 
+# which(is.na(df$Sex))
+
+x <- df$Age
+
+df %>%  mutate(cat_age = ifelse(x < 10,0, ifelse(x<20,1, ifelse(x<30, 2,ifelse(x>=30,3, 100)))))
+#--------------------------------------------------------------------------------------------------------------------------
+
+#loop
+
+x <- c(2, 6, 7, 8, 9, 10, 12.5, 24)
+
+1:length(x)
+
+
+for (j in seq_along(x)) {
+  for (i in 1:4) {
+    print(i*j)
+  }
+}
+#__________________________________________________________________________________________________________________________
+
+sq_x <- function(x, y){
+  return (x*y)
+}
+
+sq_x(4, 3)
+#____________________________________________________________________________________________________________________________
+
+df_new <- df %>% select(Age, SibSp, Parch)
+
+apply(df_new, 2, mean, na.rm = TRUE)
 
 
 
 
-
-
-
+       
